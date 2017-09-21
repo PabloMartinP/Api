fffd7e268d63d21e07fca809946ad5dac6264302
Martin Guixe <martin.guixe@hotmail.com>
2017-09-21 13:35:04 -0300

    Notificaciones

---
+++ Netmefy.Api/Controllers/api/notificacionesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Netmefy.Data;
+
+namespace Netmefy.Api.Controllers.api
+{
+    public class notificacionesController : ApiController
+    {
+        private NETMEFYEntities db = new NETMEFYEntities();
+
+        // GET: api/notificaciones
+        public IQueryable<bt_notificaciones> Getbt_notificaciones()
+        {
+            return db.bt_notificaciones;
+        }
+
+        // GET: api/notificaciones/5
+        [ResponseType(typeof(bt_notificaciones))]
+        public IHttpActionResult Getbt_notificaciones(int cliente_sk,int usuario_sk)
+        {
+            Service.NotificacionesService ns = new Service.NotificacionesService();
+
+            var notificaciones = ns.buscarNotificacionesXClienteUsuario(cliente_sk,usuario_sk);
+
+            return Ok(notificaciones);
+        }
+
+        //[ResponseType(typeof(bt_notificaciones))]
+        //public IHttpActionResult Getbt_notificaciones(int cliente_sk)
+        //{
+        //    bt_notificaciones bt_notificaciones = db.bt_notificaciones.Find(id);
+        //    if (bt_notificaciones == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(bt_notificaciones);
+        //}
+
+        // PUT: api/notificaciones/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putbt_notificaciones(int id, bt_notificaciones bt_notificaciones)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bt_notificaciones.usuario_sk)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(bt_notificaciones).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!bt_notificacionesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/notificaciones
+        [ResponseType(typeof(bt_notificaciones))]
+        public IHttpActionResult Postbt_notificaciones(bt_notificaciones bt_notificaciones)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.bt_notificaciones.Add(bt_notificaciones);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (bt_notificacionesExists(bt_notificaciones.usuario_sk))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = bt_notificaciones.usuario_sk }, bt_notificaciones);
+        }
+
+        // DELETE: api/notificaciones/5
+        [ResponseType(typeof(bt_notificaciones))]
+        public IHttpActionResult Deletebt_notificaciones(int id)
+        {
+            bt_notificaciones bt_notificaciones = db.bt_notificaciones.Find(id);
+            if (bt_notificaciones == null)
+            {
+                return NotFound();
+            }
+
+            db.bt_notificaciones.Remove(bt_notificaciones);
+            db.SaveChanges();
+
+            return Ok(bt_notificaciones);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool bt_notificacionesExists(int id)
+        {
+            return db.bt_notificaciones.Count(e => e.usuario_sk == id) > 0;
+        }
+    }
+}
---
+++ Netmefy.Service/NotificacionesService.cs
@@ -0,0 +1,106 @@
+﻿using Netmefy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Netmefy.Service
+{
+    public class NotificacionesService
+    {
+        private NETMEFYEntities db = new NETMEFYEntities();
+
+        public List<Data.bt_notificaciones> buscarNotificacionesXCliente(int cliente_sk)
+        {
+            var j = db.bt_notificaciones.Where(x => x.cliente_sk==cliente_sk).ToList();
+            return j;
+        }
+
+        public List<Data.bt_notificaciones> buscarNotificacionesXClienteUsuario(int cliente_sk, int usuario_sk)
+        {
+            var j = db.bt_notificaciones.Where(x => x.cliente_sk == cliente_sk && x.usuario_sk==usuario_sk).ToList();
+            return j;
+        }
+
+
+
+
+
+
+
+        //public Data.cliente buscar(string username)
+        //{
+        //    //Data.cliente client_found = null;
+        //    LoginService ls = new LoginService();
+        //    var dataLogin = ls.findByUsername(username);
+
+
+        //    var client_found = db.clientes.Find(dataLogin.SK);
+
+        //    return client_found;
+        //}
+
+        //public Data.cliente buscarById(int id)
+        //{
+        //    return db.clientes.Find(id);
+        //}
+
+        //public Data.router getRouterFromClient(int client_sk)
+        //{
+        //    var router_found = db.routers.Where(x => x.cliente_sk == client_sk && x.router_activo == 1).FirstOrDefault();
+
+        //    return router_found;
+        //}
+
+        //public List<dispositivo> getDevices(int cliente_sk, int router_sk)
+        //{
+        //    var devices = db.dispositivos.Where(x => x.cliente_sk == cliente_sk && x.router_sk == router_sk).ToList();
+
+        //    return devices;
+        //}
+
+        //public bool saveUser(Data.usuario usuario)
+        //{
+        //    if (!existUser(usuario.usuario_email))
+        //    {
+
+        //        int? newUsuario_sk = db.usuarios.Where(x => x.cliente_sk == usuario.cliente_sk).Max(d => d.usuario_sk);
+
+        //        if (newUsuario_sk == null)
+        //            usuario.usuario_sk = 1;
+        //        else
+        //            usuario.usuario_sk = ((int)newUsuario_sk)+1;
+
+        //        db.usuarios.Add(usuario);
+        //        db.SaveChanges();
+
+        //        //pagina p = new pagina();
+        //        //p.entidad_desc = "facebook.com";
+        //        //usuario.paginas.Add(p);
+        //        //db.SaveChanges();
+
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }            
+        //}
+
+        //public bool existUser(string email)
+        //{
+        //    return findUserByEmail(email) != null;
+        //}
+
+        //public Data.usuario findUserByEmail(string email)
+        //{
+        //    var user_found = db.usuarios.Where(x => x.usuario_email.ToLower().Equals(email.ToLower())).FirstOrDefault();
+        //    return user_found;
+        //}
+
+        //public Data.usuario findUserById(int client_sk, int usuario_sk)
+        //{
+        //    return db.usuarios.Where(x => x.cliente_sk == client_sk && x.usuario_sk == usuario_sk).FirstOrDefault();
+        //}
+    }
+}
