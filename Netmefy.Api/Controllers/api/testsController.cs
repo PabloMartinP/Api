2b98bed7ab8bf2822d0e4e6a3e67624f3b7362cf
Martin Guixe <martin.guixe@hotmail.com>
2017-10-02 17:57:54 -0300

    test

---
+++ Netmefy.Api/Controllers/api/testsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Netmefy.Data;
+
+namespace Netmefy.Api.Controllers.api
+{
+    public class testsController : ApiController
+    {
+        private NETMEFYEntities db = new NETMEFYEntities();
+
+        // GET: api/tests/5
+        [ResponseType(typeof(List<Models.testsModel>))]
+        public IHttpActionResult GetTests(int id)
+        {
+            List<bt_tests> tests = db.bt_tests.Where(x => x.cliente_sk == id).ToList();
+            if (tests == null)
+            {
+                return NotFound();
+            }
+
+            List<Models.testsModel> modelTests = Models.testsModel.ListConvertTo(tests);
+
+            return Ok(modelTests);
+        }
+        
+        // POST: api/tests
+        [ResponseType(typeof(Models.testsModel))]
+        public IHttpActionResult PostTest(Models.testsModel test)
+        {
+            bt_tests testBD = Models.testsModel.ConvertToBD(test);
+
+            db.bt_tests.Add(testBD);
+            db.SaveChanges();
+
+            test.test_id = testBD.test_id;
+            
+            return CreatedAtRoute("DefaultApi", new { id = test.test_id }, test);
+        }
+        
+    }
+}
---
+++ Netmefy.Api/Models/testsModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Netmefy.Api.Models
+{
+    public class testsModel
+    {
+
+        public int test_id { get; set; }
+        public int cliente_sk { get; set; }
+        public int? ot_id { get; set; }
+        public string tiempo_sk { get; set; }
+        public int flag_instalacion { get; set; }
+        public string comentario { get; set; }
+        public int? vel_mb_medidos { get; set; }
+        public int? potencia_recep { get; set; }
+        public int? flag_cableado_nuevo { get; set; }
+        public int? flag_modem_ok { get; set; }
+
+        public static List<testsModel> ListConvertTo(List<Data.bt_tests> tests)
+        {
+            List<testsModel> list = new List<testsModel>();
+
+            foreach (Data.bt_tests t in tests)
+            {
+                list.Add(ConvertTo(t));
+            }
+
+            return list;
+        }
+
+        public static testsModel ConvertTo(Data.bt_tests test)
+        {
+            testsModel t = new testsModel();
+
+            t.test_id = test.test_id;
+            t.cliente_sk = test.cliente_sk;
+            t.ot_id = test.ot_id;
+            t.tiempo_sk = test.tiempo_sk.ToString("dd-MM-yyyy");
+            t.flag_instalacion = test.flag_instalacion;
+            t.comentario = test.comentario;
+
+            t.vel_mb_medidos = test.vel_mb_medidos;
+            t.potencia_recep = test.potencia_recep;
+            t.flag_cableado_nuevo = test.flag_clabeado_nuevo;
+            t.flag_modem_ok = test.flag_modem_ok;
+
+            return t;
+        }
+
+        public static Data.bt_tests ConvertToBD(testsModel test)
+        {
+            Data.bt_tests t = new Data.bt_tests();
+
+            //t.test_id = test.test_id;
+            t.cliente_sk = test.cliente_sk;
+            //t.ot_id = test.ot_id;
+            if (test.tiempo_sk != null)
+                t.tiempo_sk = DateTime.ParseExact(test.tiempo_sk, "dd-MM-yyyy", null);
+            else
+                t.tiempo_sk = DateTime.Today;
+            t.flag_instalacion = test.flag_instalacion;
+            t.comentario = test.comentario;
+            t.vel_mb_medidos = test.vel_mb_medidos;
+            t.potencia_recep = test.potencia_recep;
+            t.flag_clabeado_nuevo = test.flag_cableado_nuevo;
+            t.flag_modem_ok = test.flag_modem_ok;
+            
+            return t;
+        }
+
+
+    }
+}
