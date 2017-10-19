40e906132b2cf8b3ca5cd1efd00be93a837d3434
MartinPeñaloza <pablo.penialoza@gmail.com>
2017-10-19 12:50:01 -0300

    agregado vista vw_statsxzona

--- Netmefy.Data/Model1.Context.cs
+++ Netmefy.Data/Model1.Context.cs
@@ -54,5 +54,6 @@ namespace Netmefy.Data
         public virtual DbSet<vw_isp_ots> vw_isp_ots { get; set; }
         public virtual DbSet<vw_notificaciones> vw_notificaciones { get; set; }
         public virtual DbSet<vw_porc_vel> vw_porc_vel { get; set; }
+        public virtual DbSet<vw_stats_x_zona> vw_stats_x_zona { get; set; }
     }
 }
