c5e972ea39e016d17987da046d64db22bb1e94a0
Martin Guixe <martin.guixe@hotmail.com>
2017-11-10 10:53:18 -0300

    OT y OS unicas

---
+++ Netmefy.Api/Controllers/api/ISP_OssUnitController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Netmefy.Data;
+
+namespace Netmefy.Api.Controllers.api
+{
+    public class ISP_OssUnitController : ApiController
+    {
+        private NETMEFYEntities db = new NETMEFYEntities();
+               
+        // GET: api/ISP_Oss/5
+        [ResponseType(typeof(vw_isp_oss))]
+        public IHttpActionResult Getvw_isp_oss_unit(int id)
+        {
+            vw_isp_oss vw_isp_oss = db.vw_isp_oss.Where(x => x.id == id).FirstOrDefault();
+            if (vw_isp_oss == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vw_isp_oss);
+        }
+        
+    }
+}
---
+++ Netmefy.Api/Controllers/api/ISP_OtsUnitController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Netmefy.Data;
+
+namespace Netmefy.Api.Controllers.api
+{
+    public class ISP_OtsUnitController : ApiController
+    {
+        private NETMEFYEntities db = new NETMEFYEntities();
+
+        // GET: api/ISP_Ots/5
+        [ResponseType(typeof(vw_isp_ots))]
+        public IHttpActionResult Getvw_isp_ots_unit(int id)
+        {
+            vw_isp_ots vw_isp_ots = db.vw_isp_ots.Where(x=> x.id == id).FirstOrDefault();
+            if (vw_isp_ots == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vw_isp_ots);
+        }
+
+   }
+}
